59dc779f5040fc39fe499f304f81fb49332bf8b2
dauerndamprogramieren <haeusslerludwig@gmail.com>
2024-06-11 12:44:14 +0200

    headertag added

--- js/header.js
+++ js/header.js
@@ -1,8 +1,6 @@
-function started(){
-    let UA = navigator.userAgent;
-    if(UA.match(/Android/) || UA.match(/iPhone/) || UA.match(/BlackBerry/) || UA.match(/iPod/) || UA.match(/Windows Phone/) || UA.match(/iPad/) || UA.match(/webOS/)) 
-    {
-        var placeholder = document.getElementById("placeholder-header")
-        placeholder.style.height = 100 +"px";
-    }
+//window.onscroll = scolled();
+
+function scolled() {
+    alert("test")
+    var header = document.getElementById("page-header");
 }
