bee8bff4479464d4dfa0954e511d57d610149182
dauerndambauen <haeusslerludwig@gmail.com>
2024-07-09 14:35:34 +0200

    flexbox added

--- components/index/handy.js
+++ components/index/handy.js
@@ -1,17 +1,17 @@
-import waitForElm from "/modules/waitForElemnt.js";
-await waitForElm('#mainindex');
-//above used to wait until page is loaded
+// import waitForElm from "/modules/waitForElemnt.js";
+// await waitForElm('#mainindex');
+// //above used to wait until page is loaded
 
-var tester = document.getElementById("headerfile");
-var projectList = document.getElementById("projects-list");
-var projectsShip = document.getElementById("orange-boat");
+// var tester = document.getElementById("headerfile");
+// var projectList = document.getElementById("projects-list");
+// var projectsShip = document.getElementById("orange-boat");
 
-if(tester.offsetHeight >=127){
-    var grid = document.getElementById("mainindex");
-    var gridHeight = document.getElementById("orange-boat").offsetHeight;
-    grid.style.gridTemplateColumns = "auto";
-    grid.style.gridTemplateRows = "auto auto";
-    projectsShip.style.width = "300px";
-    projectList.style.height = "90%";
-    document.getElementById("list").style.height = "10%"
-}
+// if(tester.offsetHeight >=127){
+//     var grid = document.getElementById("mainindex");
+//     var gridHeight = document.getElementById("orange-boat").offsetHeight;
+//     grid.style.gridTemplateColumns = "auto";
+//     grid.style.gridTemplateRows = "auto auto";
+//     projectsShip.style.width = "300px";
+//     projectList.style.height = "90%";
+//     document.getElementById("list").style.height = "10%"
+// }
